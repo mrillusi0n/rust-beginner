cc9c3ac436bff390fcd633fa371ceeac27f63fa6
Nikhil CSB <csbnikhil@gmail.com>
2020-07-13 16:15:39 +0530

    yay Option!

--- binary_search_r.rs
+++ binary_search_r.rs
@@ -1,25 +1,32 @@
-fn search(k: i32, arr: &[i32], low: usize, high: usize) -> bool {
+fn _search(k: i32, arr: &[i32], low: usize, high: usize) -> Option<usize> {
+    let found: Option<usize>;
     let mid = (low + high) / 2;
 
     if low > high {
-        return false;
+        return None;
     }
 
     if k == arr[mid] {
-        return true;
+        found = Some(mid);
     } else if k > arr[mid] {
-        return search(k, arr, mid + 1, high);
+        found = _search(k, arr, mid + 1, high);
     } else {
-        return search(k, arr, low, mid - 1);
+        found = _search(k, arr, low, mid - 1);
     }
+
+    found
+}
+
+fn search(k: i32, arr: &[i32], size: usize) -> Option<usize> {
+    _search(k, arr, 0, size - 1)
 }
 
 fn main() {
     let nums = [3, 4, 5, 6, 7, 8];
     let num = 7;
 
-    if search(num, &nums, 0, 5) {
-        println!("{} was found :D", num);
+    if let Some(i) = search(num, &nums, 6) {
+        println!("{} was found at {} :D", num, i);
     } else {
         println!("{} was not found :[", num);
     }
