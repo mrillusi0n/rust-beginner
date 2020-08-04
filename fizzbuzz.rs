3dfdfcc83ee10d0d49eb201f750e20d50b14b76f
Nikhil CSB <csbnikhil@gmail.com>
2020-08-04 16:08:31 +0530

    help from @kangalioo

---
+++ fizzbuzz.rs
@@ -0,0 +1,23 @@
+fn fizzbuzz(n: u32) -> String {
+    let mut fin = String::new();
+
+    let speaks = [(3, "Fizz"), (5, "Buzz")];
+
+    for (num, speak) in &speaks {
+        if n % num == 0 {
+            fin += speak;
+        }
+    }
+
+    if fin.is_empty() {
+        fin = n.to_string();
+    }
+
+    fin
+}
+
+fn main() {
+    for i in 0..16 {
+        println!("{}", fizzbuzz(i));
+    }
+}
